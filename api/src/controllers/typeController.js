9adc65ad7477c3cb8744096b343e885c6ea4f883
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-27 15:50:38 -0300

    Fue creado el endpoint que devuelve todos los types.

--- api/src/controllers/typeController.js
+++ api/src/controllers/typeController.js
@@ -5,6 +5,15 @@ class TypeController {
         this.typeService = new TypeService('https://pokeapi.co/api/v2/type');
     }
 
+    getAllTypesDB = async (req, res) => {
+        try {
+            const types = await this.typeService.getAllTypesDB();
+            return res.json(types);
+        } catch (error) {
+            return res.status(500).send(error.message);
+        }
+    }
+
     loadTypesTable = async (req, res) => {
         try {
             const types = await this.typeService.getAllTypes();
