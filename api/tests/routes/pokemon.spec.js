755d6666676700a26e7dabec750018ed96acbe4b
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-30 16:11:42 -0300

    Fue realizado tests para las rutas: /pokemons /pokemons/name

--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -5,20 +5,40 @@ const app = require('../../src/app.js');
 const { Pokemon, conn } = require('../../src/db.js');
 
 const agent = session(app);
-const pokemon = {
-  name: 'Pikachu',
-};
+
 
 describe('Pokemon routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Pokemon.sync({ force: true })
-    .then(() => Pokemon.create(pokemon)));
-  describe('GET /pokemons', () => {
-    it('should get 200', () =>
-      agent.get('/pokemons').expect(200)
-    );
-  });
+    before(() => conn.authenticate()
+        .catch((err) => {
+            console.error('Unable to connect to the database:', err);
+        }));
+
+    beforeEach(() => Pokemon.sync({ force: true })
+        .then(() => Pokemon.create({
+            name: 'Pikachu',
+            hp: 50,
+            attack: 40,
+            defense: 30,
+            speed: 60,
+            height: 1,
+            weight: 6,
+        })));
+
+
+    describe('GET /pokemons', () => {
+        it('should get 200', () =>
+            agent.get('/pokemons').expect(200)
+        );
+    });
+
+    describe('GET /pokemons/name', () => {
+        it('should get 400 if name is not provided', () =>
+            agent.get('/pokemons/name').expect(400)
+        );
+
+        it('should get 200 with valid name', () =>
+            agent.get('/pokemons/name?name=Pikachu').expect(200)
+        );
+    });
+
 });
