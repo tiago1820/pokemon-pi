cbe206a154d0251d8ef760d624ef27af3e5d7427
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-12-03 00:14:25 -0300

    Implementando nuevo estilo al componente Detail.

--- client/src/app/components/Detail/Detail.jsx
+++ client/src/app/components/Detail/Detail.jsx
@@ -17,44 +17,53 @@ export const Detail = () => {
     const renderBar = (label, value) => {
         const width = (value / 100) * 100; // Rango: 0 a 100
         return (
-          <div className={styles.barContainer} key={label}>
-            <div className={styles.barLabel}>{label}</div>
-            <div className={styles.bar}>
-              <div className={styles.barFill} style={{ width: `${width}%` }} />
-              <div className={styles.barValue}>{value}</div>
+            <div className={styles.barContainer} key={label}>
+                <div className={styles.barLabel}>{label}</div>
+                <div className={styles.bar}>
+                    <div className={styles.barFill} style={{ width: `${width}%` }} />
+                    <div className={styles.barValue}>{value}</div>
+                </div>
             </div>
-          </div>
         );
-      };
+    };
 
     return (
         <div className={styles.container}>
             <div className={styles.card}>
                 {/* id de la imagen */}
-                <img className={styles.img} src={pokemonImages[pokemon.id] || defaultImg} alt="" />
+                <div className={styles.row}>
+                    <div className={styles.left}>
+                        <img className={styles.img} src={pokemonImages[pokemon.id] || defaultImg} alt="" />
 
-                <div className={styles.cardContent}>
-                    <div className={styles.name}>{capitalizeFirstLetter(pokemon.name)}</div>
-                </div>
-                <div className={styles.attributes}>
-                    {renderBar('HP', pokemon.hp)}
-                    {renderBar('Attack', pokemon.attack)}
-                    {renderBar('Defense', pokemon.defense)}
-                    {renderBar('Speed', pokemon.speed)}
-                    {/* {renderBar('Height', pokemon.height)}
-                    {renderBar('Weight', pokemon.weight)} */}
-                    <div>Height: {pokemon.height}</div>
-                    <div>Weight: {pokemon.weight}</div>
+                        <div className={styles.types}>
+                            {pokemon.types &&
+                                pokemon.types.map((type, index) => (
+                                    <span key={index} className={`${styles.type} ${styles[type.toLowerCase()]}`}>
+                                        {type}
+                                    </span>
+                                ))}
+                        </div>
+                    </div>
+
+                    <div className={styles.right}>
+                        {/* <div className={styles.cardContent}>
+    <div className={styles.name}>{capitalizeFirstLetter(pokemon.name)}</div>
+</div> */}
+                        <div className={styles.attributes}>
+                            {renderBar('HP', pokemon.hp)}
+                            {renderBar('Attack', pokemon.attack)}
+                            {renderBar('Defense', pokemon.defense)}
+                            {renderBar('Speed', pokemon.speed)}
+                            {/* {renderBar('Height', pokemon.height)}
+    {renderBar('Weight', pokemon.weight)} */}
+                            <div>Height: {pokemon.height}</div>
+                            <div>Weight: {pokemon.weight}</div>
+                        </div>
+
+                    </div>
                 </div>
 
-                <div className={styles.types}>
-                    {pokemon.types &&
-                        pokemon.types.map((type, index) => (
-                            <span key={index} className={`${styles.type} ${styles[type.toLowerCase()]}`}>
-                                {type}
-                            </span>
-                        ))}
-                </div>
+
             </div>
 
         </div>
