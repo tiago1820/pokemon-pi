f5794a80f34bda98afacc3b4e49002a42e6e61a6
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-12-15 10:51:33 -0300

    deploy test

--- api/src/app.js
+++ api/src/app.js
@@ -12,7 +12,7 @@ const server = express();
 
 server.name = 'API';
 
-const whiteList = ['http://localhost:3000', 'http://localhost:3001'];
+const whiteList = ['http://15.229.85.84:3000', 'http://15.229.85.84:3001'];
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
@@ -22,7 +22,7 @@ server.use(cors({
   origin: whiteList
 }));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://15.229.85.84:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
