bd5606347709a5c3ef45fa64078cb7708f655370
tiago1820 <tiago.zdo@gmail.com>
2023-12-30 14:39:45 -0300

    Refact insertTypesInDB

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,17 +2,14 @@ const { Router } = require('express');
 const router = Router();
 
 const PokemonController = require('../controllers/pokemonController');
-
 const pokeController = new PokemonController()
 
 router.get('/pokemons/name', (req, res) => pokeController.getPokemonByName(req, res));
 router.get('/pokemons', (req, res) => pokeController.getAllPokemons(req, res));
 router.get('/pokemons/:id', (req, res) => pokeController.getPokemonById(req, res));
 router.post('/pokemons', (req, res) => pokeController.postPokemon(req, res));
-router.delete('/pokemons/:id', (req, res) => pokeController.removePokemonById(req, res));
 router.put('/pokemons/:id', (req, res) => pokeController.updatePokemon(req, res));
-
+router.delete('/pokemons/:id', (req, res) => pokeController.removePokemonById(req, res));
 router.get('/types', (req, res) => pokeController.getAllTypes(req, res));
-// router.get('/types/load', (req, res) => pokeController.loadTypesInTable(req, res));
 
 module.exports = router;
