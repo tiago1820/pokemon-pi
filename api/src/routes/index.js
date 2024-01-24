31116dc9d3d3dd18609491be2699f025a8321e89
tiago1820 <tiago.zdo@gmail.com>
2024-01-24 11:59:01 -0300

    The routes: /pokemons/name /pokemons/{id} /pokemons are documented.

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,8 +5,72 @@ const PokemonController = require('../controllers/pokemonController');
 const pokeController = new PokemonController()
 
 router.get('/pokemons/name', (req, res) => pokeController.getPokemonByName(req, res));
-router.get('/pokemons', (req, res) => pokeController.getAllPokemons(req, res));
+/**
+ * @swagger
+ * /pokemons/name:
+ *   get:
+ *     summary: Obtener un pokemon por nombre
+ *     tags:
+ *       - Pokemons
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         required: true
+ *         description: Nombre del pokemon que deseas obtener
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Un objeto con todos los datos del pokemon.
+ *       404:
+ *         description: Pokemon no encontrado.
+ *       400:
+ *         description: Falta el nombre del pokemon.
+ *       500:
+ *         description: Error interno del servidor.
+ */
+
 router.get('/pokemons/:id', (req, res) => pokeController.getPokemonById(req, res));
+/**
+ * @swagger
+ * /pokemons/{id}:
+ *   get:
+ *     summary: Obtener un pokemon por nombre
+ *     tags:
+ *       - Pokemons
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Name del pokemon que deseas obtener
+ *         schema:
+ *           type: number
+ *     responses:
+ *       200:
+ *         description: Un objeto con todos los datos del pokemon.
+ *       404:
+ *         description: Pokemon no encontrado.
+ *       400:
+ *         description: Falta el nombre del pokemon.
+ *       500:
+ *         description: Error interno del servidor.
+ */
+
+router.get('/pokemons', (req, res) => pokeController.getAllPokemons(req, res));
+/**
+ * @swagger
+ * /pokemons:
+ *   get:
+ *     summary: Obtener todos los pokemons
+ *     tags:
+ *       - Pokemons
+ *     responses:
+ *       200:
+ *         description: Lista de todos los pokemons.
+ *       500:
+ *         description: Error interno del servidor.
+ */
+
 router.post('/pokemons', (req, res) => pokeController.postPokemon(req, res));
 router.put('/pokemons/:id', (req, res) => pokeController.updatePokemon(req, res));
 router.delete('/pokemons/:id', (req, res) => pokeController.removePokemonById(req, res));
